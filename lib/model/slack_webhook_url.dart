7cea613db72d2afe0ae1937f997cbc728097ae72
idonun <mc.natsu831@gmail.com>
2022-11-03 23:54:09 +0900

    Add maskedUrl to SlackWebhookUrl

--- lib/model/slack_webhook_url.dart
+++ lib/model/slack_webhook_url.dart
@@ -9,4 +9,15 @@ class SlackWebhookUrl with _$SlackWebhookUrl {
   }) = _SlackWebhookUrl;
 
   const SlackWebhookUrl._();
+
+  String maskedUrl() {
+    try {
+      final url = Uri.parse(value);
+      final splitedPath = url.path.split('/');
+      final path = splitedPath.isNotEmpty && splitedPath.length > 1 ? splitedPath[1] : '';
+      return 'https://${url.host}/$path/*****${value.substring(value.length - 4)}';
+    } on FormatException catch (_) {
+      return '';
+    }
+  }
 }
